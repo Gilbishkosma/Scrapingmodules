8cacb4b553c2aa37233ed4e62506784c66e5e3a5
Gilbishkosma <Gilbeeshkosma15@gmail.com>
2019-01-13 02:54:13 +0530

    [half six degrees of Kevin_Bacon]

--- Kevin_Bacon.py
+++ Kevin_Bacon.py
@@ -6,12 +6,19 @@ Used to get all the links in the wiki page of Kevin Bacon.
 import requests
 from bs4 import BeautifulSoup
 import re
+import datetime
+import random
 
-url = "http://en.wikipedia.org/wiki/Kevin_Bacon"
+random.seed(datetime.datetime.now()) #so that it will give different number every time
 
-req = requests.get(url)
+def getLinks(articleUrl):
+	req = requests.get("http://en.wikipedia.org" + articleUrl)
+	soup = BeautifulSoup(req.text,'html.parser')
+	return soup.find('div',{'id':'bodyContent'}).find_all('a',href=re.compile("^(/wiki/)((?!:).)*$"))
 
-soup = BeautifulSoup(req.text,'html.parser')
-for link in soup.find('div',id="bodyContent").find_all('a',href=re.compile("^(/wiki/)((?!:).)*$")):
-	if 'href' in link.attrs:
-		 print(link.attrs['href'])
+links = getLinks("/wiki/Kevin_Bacon") #it will store all the anchor tags found in "http://en.wikipedia.org/wiki/Kevin_Bacon"
+while len(links) > 0:
+	newArticle = links[random.randint(0,len(links)-1)].attrs['href'] #it will select one anchor tag from all the present anchor tags
+	print(newArticle)
+	links = getLinks(newArticle) #again call the getLinks with a new url.
+
